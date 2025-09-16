8cd38d0a70d0c5beee236ee653c00671812c29a8
Kaushik Jadav <kaushikjadav602@gmail.com>
2025-09-16 17:23:03 +0530

    Add job store

---
+++ src/AsyncEndpoints/Job/IJobStore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncEndpoints.Job;
+
+public interface IJobStore
+{
+    Task<Job?> Get(Guid id, CancellationToken cancellationToken);
+    Task Add(Job job, CancellationToken cancellationToken);
+}
---
+++ src/AsyncEndpoints/Job/InMemoryJobStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncEndpoints.Job;
+
+public class InMemoryJobStore : IJobStore
+{
+    private readonly ConcurrentDictionary<Guid, Job> jobs = new();
+
+    public Task Add(Job job, CancellationToken cancellationToken)
+    {
+        jobs.TryAdd(job.Id, job);
+        return Task.CompletedTask;
+    }
+
+    public Task<Job?> Get(Guid id, CancellationToken cancellationToken)
+    {
+        jobs.TryGetValue(id, out var job);
+        return Task.FromResult(job);
+    }
+}
