6bccdefcd54aa3f5bcc10048ca0d2879188aa3e9
Kaushik Jadav <kaushikjadav602@gmail.com>
2025-09-16 17:23:57 +0530

    Update ServiceCollectionExtensions for job store

--- src/AsyncEndpoints/ServiceCollectionExtensions.cs
+++ src/AsyncEndpoints/ServiceCollectionExtensions.cs
@@ -1,3 +1,6 @@
+using AsyncEndpoints.AsyncEndpointRequestHandler;
+using AsyncEndpoints.Configurations;
+using AsyncEndpoints.Job;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AsyncEndpoints;
@@ -7,6 +10,13 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddAsyncEndpoints(this IServiceCollection services)
     {
         services.AddSingleton<AsyncEndpointConfig>();
+        services.AddSingleton<IJobStore, InMemoryJobStore>();
+        return services;
+    }
+
+    public static IServiceCollection AddAsyncEndpointHandler<TAsyncEndpointRequestHandler, TRequest, TResponse>(this IServiceCollection services, string jobName) where TAsyncEndpointRequestHandler : class, IAsyncEndpointRequestHandler<TRequest, TResponse>
+    {
+        services.AddKeyedScoped<IAsyncEndpointRequestHandler<TRequest, TResponse>, TAsyncEndpointRequestHandler>(jobName);
         return services;
     }
 }
