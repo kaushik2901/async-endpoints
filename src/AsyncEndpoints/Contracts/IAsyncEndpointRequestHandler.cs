2c4b76555d18eb77f1d0f8c085145578f4f7eebf
Kaushik Jadav <kaushikjadav602@gmail.com>
2025-09-16 17:23:39 +0530

    Add IAsyncEndpointRequestHandler

---
+++ src/AsyncEndpoints/Contracts/IAsyncEndpointRequestHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AsyncEndpoints.Context;
+
+namespace AsyncEndpoints.AsyncEndpointRequestHandler;
+
+public interface IAsyncEndpointRequestHandler<TRequest, TResponse>
+{
+    Task<TResponse> HandleAsync(AsyncContext<TRequest> context, CancellationToken token);
+}
