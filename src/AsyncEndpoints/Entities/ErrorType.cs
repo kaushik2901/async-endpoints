c48bbb531a0f5770acb04101eeab2554d125d2c1
Kaushik Jadav <kaushikjadav602@gmail.com>
2025-09-23 16:20:58 +0530

    Add ErrorType and ErrorClassifier

---
+++ src/AsyncEndpoints/Entities/ErrorType.cs
@@ -0,0 +1,8 @@
+﻿namespace AsyncEndpoints.Entities;
+
+public enum ErrorType
+{
+    Transient = 100,   // Network timeouts, temporary service unavailability
+    Permanent = 200,   // Invalid arguments, business logic errors
+    Retriable = 300    // Unknown errors that might succeed on retry
+}
---
+++ src/AsyncEndpoints/Utilities/ErrorClassifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AsyncEndpoints.Entities;
+
+namespace AsyncEndpoints.Utilities;
+
+public static class ErrorClassifier
+{
+    public static ErrorType Classify(Exception ex)
+    {
+        return ex switch
+        {
+            TaskCanceledException => ErrorType.Transient,
+            TimeoutException => ErrorType.Transient,
+            HttpRequestException httpEx when httpEx.Message.Contains("timeout") => ErrorType.Transient,
+            ArgumentException => ErrorType.Permanent,
+            InvalidOperationException => ErrorType.Permanent,
+            _ => ErrorType.Retriable
+        };
+    }
+}
