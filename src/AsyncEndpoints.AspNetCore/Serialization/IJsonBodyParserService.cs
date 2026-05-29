f761900f6acbb50afa94fcf73c3c359c0d4405b0
Kaushik Jadav <kaushikjadav602@gmail.com>
2026-05-29 12:55:30 +0530

    Remvoe JsonBodyParser

--- src/AsyncEndpoints.AspNetCore/Serialization/IJsonBodyParserService.cs
+++
@@ -1,9 +0,0 @@
-using AsyncEndpoints.Abstractions.Common;
-using Microsoft.AspNetCore.Http;
-
-namespace AsyncEndpoints.AspNetCore.Serialization;
-
-public interface IJsonBodyParserService
-{
-	Task<MethodResult<T?>> ParseAsync<T>(HttpContext httpContext, CancellationToken cancellationToken = default);
-}
